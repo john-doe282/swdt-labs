6ef389f337e4473b228fc4e49f36875b7eaf4282
john-doe282 <molnar2@icloud.com>
2021-03-31 00:25:53 +0300

    extra task 1 done

--- src/com/andrew/lab_5/Main.java
+++ src/com/andrew/lab_5/Main.java
@@ -1,14 +1,15 @@
 package com.andrew.lab_5;
 
+import com.andrew.lab_5.extra_tasks.ExtraTask1;
 import com.andrew.lab_5.tasks.*;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void mainTasks() {
         System.out.println(Task1.getIntegersFromList(List.of(1, "a", 2, "b")));
 
         System.out.println(Task2.firstNonRepeatedLetter("sTrRess"));
@@ -21,6 +22,15 @@ public class Main {
 
         System.out.println(Task5.meeting("Fred:Corwill;Wilfried:" +
                 "Corwill;Barney:Tornbull;Bjorn:Tornbull"));
+    }
+
+    public static void extraTasks() {
+        System.out.println(ExtraTask1.nextBigger(2017));
+    }
+    public static void main(String[] args) {
+	// write your code here
+        mainTasks();
+        extraTasks();
 
     }
 }
--- src/com/andrew/lab_5/extra_tasks/ExtraTask1.java
+++ src/com/andrew/lab_5/extra_tasks/ExtraTask1.java
@@ -1,4 +1,37 @@
 package com.andrew.lab_5.extra_tasks;
 
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
 public class ExtraTask1 {
+    public static Integer nextBigger(Integer num) {
+        List<Character> numList = num.toString().
+                chars().
+                mapToObj(c -> (char) c).
+                collect(Collectors.toList());
+
+        for (int i = numList.size() - 1; i > -1; i--) {
+            int before = numList.get(i);
+            List<Character> subNumList = numList.subList(i, numList.size());
+            subNumList.sort(Comparator.reverseOrder());
+
+            if (numList.get(i) != before) {
+                break;
+            }
+        }
+
+        String nextBiggerStr = numList.stream().
+                map(Objects::toString).
+                collect(Collectors.joining());
+
+        Integer nextBigger = Integer.valueOf(nextBiggerStr);
+
+        if (nextBigger > num) {
+            return nextBigger;
+        }
+        return -1;
+    }
 }
